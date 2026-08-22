4456633dd0f13ba783301bc6a2f0d02e7595b901
toby" radiation "fox <166326616+radishcatish@users.noreply.github.com>
2026-08-22 09:18:29 -0400

    update design, optimize

--- script.lua
+++ script.lua
@@ -1,8 +1,8 @@
 
 local function meow()
     animations.model.communicate:restart()
-    for i=1,5 do sounds:stopSound("meow" .. i) end
-    sounds["meow" .. tostring(math.random(1, 5))]
+    for i=1,2 do sounds:stopSound("meow" .. i) end
+    sounds["meow" .. tostring(math.random(1, 2))]
         :setPitch(math.random(75, 125) / 100)
         :setPos(models.model.Head:partToWorldMatrix():apply())
         :setSubtitle("Radi meows")
@@ -10,7 +10,7 @@ local function meow()
 end
 function pings.meowmsg()
     animations.model.message:restart()
-    for i=1,5 do sounds:stopSound("meow" .. i) end
+    for i=1,2 do sounds:stopSound("meow" .. i) end
     sounds["text"]
         :setPitch(math.random(75, 125) / 100)
         :setPos(models.model.Head:partToWorldMatrix():apply())
@@ -32,14 +32,14 @@ end
 
 vanilla_model.HAT:setVisible(false)
 vanilla_model.CAPE:setVisible(false)
-
+vanilla_model.JACKET:setVisible(false)
 models.model.Head:setSecondaryTexture("CUSTOM", textures["star"])
-
+animations.model.tail:play()
 
 local survivalplate =
-    models:newText("health"):setScale(.25):setPos(0, 42, 0):alignment("center"):setSeeThrough(true)
+    models:newText("health"):setScale(.25):setPos(0, 42, 0):alignment("center"):background(true)
 function events.tick()
-      survivalplate:setVisible(client:getViewer():getTargetedEntity() == player or host:isHost() and not renderer:isFirstPerson())
+      survivalplate:setVisible(host:isHost() and not renderer:isFirstPerson())
 
     local hp = math.floor(player:getHealth())
     local absorption = player:getAbsorptionAmount()
@@ -48,7 +48,7 @@ function events.tick()
     local armor = player:getArmor()
 
     survivalplate:setText(toJson({
-        { text = "❤ ", color = "#ff4444" },
+        { text = "❤ ", color = "#447FFF" },
         { text = hp .. " ", color = "white" },
         { text = absorption > 0 and ("+" .. absorption .. " ") or "", color = "#ffaa00" },
         { text = "🍖 ", color = "#c8a96e" },
@@ -61,8 +61,18 @@ function events.tick()
 
 end
 
-
-function events.render()
+local tailx, taily, tailz = 0, 0, 0
+function events.render(delta)
     local r = client.getCameraRot()
     survivalplate:setRot(r.x, r.y * -1 - (player:getBodyYaw(delta) - 180 )* -1, 0)
+    local nonIdle = (1 + player:getVelocity().xz:length() * 20)
+    local idleRot = (math.sin(world.getTime(delta) / 10) * 20) / nonIdle
+ 
+
+    tailx, taily, tailz =
+    math.lerp(tailx, 30 / nonIdle, delta), 
+    math.lerp(taily, vanilla_model.LEFT_LEG:getOriginRot().x / 10 + idleRot, delta), 
+    0
+
+    models.model.Body.Tail:setRot(tailx, taily, tailz)
 end
